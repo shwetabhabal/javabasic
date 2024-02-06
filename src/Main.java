49ef1b69f96b58b3daac2a6c7e397c348d989310
unknown <bhabalshweta02@gmail.com>
2024-02-06 11:46:11 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,176 @@
+public class Main {
+    public static void main(String[] args) {
+        //If Else Statement Problems
+        //1. Read a single Digit Number and write in word
+
+        int n=9;
+        if(n==1){
+            System.out.println("one");
+        }
+        else if(n==2){
+            System.out.println("two");
+        }
+        else if(n==3){
+            System.out.println("three");
+        }
+        else if(n==4){
+            System.out.println("four");
+        }
+        else if(n==5){
+            System.out.println("five");
+        }
+        else if(n==6){
+            System.out.println("six");
+        }
+        else if(n==7){
+            System.out.println("seven");
+        }
+        else if(n==8){
+            System.out.println("eight");
+        }
+        else if(n==9){
+            System.out.println("nine");
+        }
+        else{
+            System.out.println("invalid");
+        }
+
+
+        //2. Read a Number 1,10,100,1000 and display unit, ten hundred
+
+//         int n=9999;
+//         int unit= n%10;
+//         int tens=n%100;
+//         int hund=n%1000;
+//         System.out.println("unit: "+unit);
+//         System.out.println("tens: "+tens);
+//         System.out.println("hundreds: "+hund);
+
+        //Programs With While Loop
+        //Write a Program for the sum of n natural numbers eg. Input N=5 output 1+2+3+4+5
+//         int n=2;
+//         int sum=0;
+//         while(n>0){
+//         sum=sum+n;
+//         n--;
+//         }
+//         System.out.println("sum: "+sum);
+
+
+        //Write a Program to reverse the integer number eg. Input n=231 reverse is 132
+
+//         int n=132;
+//         int rev=0;
+//         while(n>0){
+//         int temp=n%10;
+//         rev=10*rev+temp;
+//         n=n/10;
+//         }
+//         System.out.println("rev: "+rev);
+//
+
+        //Programs With For Loop
+        //Write a Program for the sum of n natural numbers eg. Input N=5 output 1+2+3+4+5
+
+//         int N=5;
+//         int sum=0;
+//         for(int i=0;i<=N;i++){
+//         sum=sum+i;
+//         }
+//         System.out.println("sum: "+sum);
+
+        // Write a Program to reverse the integer number eg. Input n=231 reverse is 132
+
+//         int n=123;
+//         int rev=0;
+//         for(int i=0;i<=n*10;i++){
+//         int temp=n%10;
+//         rev=rev*10 +temp;
+//         n=n/10;
+//         }
+//         System.out.println("reverse: "+rev);
+//
+
+        //Write a Program to find Palindrome Number
+
+//         int n=121;
+//         int num=n;
+//         int rev=0;
+//         for(int i=0;i<=n*10;i++){
+//         int temp=n%10;
+//         rev=rev*10 +temp;
+//         n=n/10;
+//         }
+//         if(num==rev){
+//         System.out.println("palindrom");
+//         }
+//         else{
+//         System.out.println("not palindrom");
+//         }
+
+
+        //Programs with Switch Statement
+        //1. Write a Program to Check Vowel or Consonant
+//         char vow='i';
+//         switch(vow){
+//         case 'a' :
+//         System.out.println("vowels");
+//         break;
+//         case 'e':
+//         System.out.println("vowels");
+//         break;
+//         case 'i':
+//         System.out.println("vowels");
+//         break;
+//         case 'o' :
+//         System.out.println("vowels");
+//         break;
+//         case 'u':
+//         System.out.println("vowels");
+//         break;
+//         default:
+//         System.out.println("invalid");
+//
+//         }
+
+        // 2. Write a Program to print the month name for the given number
+
+        // Operator Programs
+        // 1. Enter two numbers and do the following arithmetic Operations find max and min.
+
+//            int a=1;
+//            int b=2;
+//            int c=3;
+//            int op1= a+b*c;
+//            int op2=c+a/b;
+//            int op3= a%b+c;
+//            int op4= a*b+c;
+//
+//            if(op1>op2 && op1>op3 && op1>op4){
+//                System.out.println("max:"+op1);
+//            }
+//            else if(op2>op3 && op2>op3 && op2>op4){
+//                System.out.println("max:"+op2);
+//            }
+//            else if(op3>op4){
+//                System.out.println("max:"+op3);
+//            }
+//            else{
+//                System.out.println("max:"+op4);
+//            }
+//
+//            if(op1<op2 && op1<op3 && op1<op4){
+//                System.out.println("min:"+op1);
+//            }
+//            else if(op2<op3 && op2<op3 && op2<op4){
+//                System.out.println("min:"+op2);
+//            }
+//            else if(op3<op4){
+//                System.out.println("min:"+op3);
+//            }
+//            else{
+//                System.out.println("min:"+op4);
+//            }
+
+    }
+}
